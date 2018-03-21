364e1b59a51af86f9a426b3af2d861b0a089cd6d
unknown <bsa6.23.94@gmail.com>
2018-03-21 16:55:37 -0400

    i have no idea how to call the animal name i know were only working on recursion and this but i tried my hand at constructors and i genuinely dont understand how to find resources to explain it to me.

--- recursion.js
+++ recursion.js
@@ -1,15 +1,18 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
+/*let n = 1;
 while (n <= 10) {
   console.log('While Loop', n);
   n++;
-}
+}*/
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+  for(let n = 1; n <= 10; n++) {
+    console.log('While Loop', n)
+  }
 
 // when you code is ready, un-comment the next line and run the file
 // console.log(countToTen());
@@ -17,7 +20,7 @@ while (n <= 10) {
 
 // Problem 2:
 
-const factorial = n => {
+/*const factorial = n => {
   let result = 1;
   for (let i = 2; i <= n; i++) {
     result *= i;
@@ -25,9 +28,15 @@ const factorial = n => {
   return result;
 };
 
-console.log(factorial(5));
+console.log(factorial(5));*/
 
 // write the above function in a recursive way.
+function recursiveFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return (n * recursiveFactorial(n-1));
+}
 
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(5));
--- this.js
+++ this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Whenever a function is in the global scope, the this inside of the function will refer to the window object.
+
+* 2. Whenever a function is called before a dot use this as the object.
+* 3. When a constructor is used (this) is the instance of the object within that constructor like within new the this is a new object within the function.
+* 4. When Javascript uses call or apply, this is defined explicitly.
 *
 * write out a code example of each explanation above
 */
@@ -14,15 +15,34 @@ console.log('hello world!');
 // Principle 1
 
 // code example for Window Binding
+function global() {
+    console.log(this)
+}
 
 // Principle 2
 
 // code example for Implicit Binding
+function kitten(name) {
+    this.name = poo;
+}
 
 // Principle 3
 
 // code example for New Binding
+ function poop() {
+     this.name = "fecal material";
+     console.log(this);
+ }
+ new poop();
 
 // Principle 4
 
 // code example for Explicit Binding
+
+function tapap() {
+    console.log(this);
+}
+var topop = {
+    value: 20
+};
+tapap.call(topop);
